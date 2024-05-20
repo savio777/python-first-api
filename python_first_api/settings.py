b36e70d15d99ecce57ef32d7ab71dfdc556a0cf0
savio <saviopf22@gmail.com>
2024-05-20 06:18:36 -0300

    get list data users

--- python_first_api/settings.py
+++ python_first_api/settings.py
@@ -32,7 +32,8 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware", #  remove for problem csrf_token
+    "path.to.JsonMiddleware",  # add this for get json data in requests
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
