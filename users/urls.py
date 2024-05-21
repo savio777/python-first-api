f405a0aadd968dae1c84e38a30c4d59c88f810ad
savio <saviopf22@gmail.com>
2024-05-21 18:02:47 -0300

    creae endpoint get by id

--- users/urls.py
+++ users/urls.py
@@ -2,4 +2,7 @@ from django.urls import include, path
 
 from . import views
 
-urlpatterns = [path("", views.list_all, name="list_all")]
+urlpatterns = [
+    path("", views.list_all, name="list_all"),
+    path("<str:id>", views.get_by_id, name="get_by_id"),
+]
--- users/views.py
+++ users/views.py
@@ -21,6 +21,20 @@ def list_all(req):
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(["GET"])
+def get_by_id(req, id):
+    if req.method == "GET":
+        try:
+            user = Users.objects.get(id_public=id)
+
+            serializer = UserSerializer(user)
+            return Response(serializer.data)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
 # def databaseEmDjangoEx():
 
 #     data = User.objects.get(pk='gabriel_nick')          # OBJETO
