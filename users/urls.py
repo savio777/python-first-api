4a14aeee806c9b89600481ae681346aa93e89c7f
savio <saviopf22@gmail.com>
2024-05-22 12:27:18 -0300

    types params

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 urlpatterns = [
     path("", views.list_all, name="list_all"),
-    path("<str:id>", views.get_by_id, name="get_by_id"),
+    path("<uuid:id>", views.get_by_id, name="get_by_id"),
 ]
+
+# types params-> str | int | slug | path
+# str -> Matches any non-empty string
+# int -> integer
+# slug -> ASCII string consisting of lowercase letters, numbers and hyphens (welcome-to-the-1-room)
+# uuid -> Unique Identifier (c1d19dbc-9078-4672-9291-864888ea5066)
+# path -> Matches any non-empty string, including the path separator /
